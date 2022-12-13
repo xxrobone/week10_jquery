3314839fb071cb4eeb4540709fdd588de35936cf
robonexx <robertwagar@gmail.com>
2022-12-13 10:01:22 +0100

    exercise 1+2 and extra

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 $(function () {
   console.log('jquery working');
 });
+
+$('.btn:first-of-type').on('click', function () {
+  $(this).css({
+    backgroundColor: '#fafafa',
+    color: '#282828',
+  });
+  $('h1').toggle('slow');
+  $('header').css({
+    backgroundColor: '#000',
+    color: 'aqua',
+  });
+});
+
+$('.btn:first-of-type').on({
+  mouseenter: function () {
+    $(this).css('opacity', '0.85');
+  },
+  mouseleave: function () {
+    $(this).css('opacity', '1');
+  },
+});
